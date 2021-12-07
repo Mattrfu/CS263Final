4272ae99176680f26e180b7223d084251175d9a9
David <davidseo901@gmail.com>
2021-12-06 22:39:50 -0500

    added more tests

--- hookify.js
+++ hookify.js
@@ -1,14 +1,24 @@
 // object code to be injected in first js
-var obj = {
-  "resolution" : 0,
-  "gamut" : 0
+if (!obj){
+  var obj = {
+    "resolution" : 0,
+    "gamut" : 0,
+    "color_depth" : 0,
+    "cpu" : 0,
+    "contrast" : 0,
+    "memory" : 0,
+    "concurrency" : 0,
+    "hdr" : 0,
+    "inverted_colors" : 0,
+    "color_depth" : 0
+  };
 }
 //
 
 // stat display code to be injected at the end of each js
-for (var key in obj){
-  console.log(`${key} count: ${obj[key]}`);
-}
+//for (var key in obj){
+//  console.log(`${key} count: ${obj[key]}`);
+//}
 //
 
 var fs = require('fs');
@@ -29,31 +39,58 @@ estraverse.traverse(ast, {
 });
 
 // add object declaration to beginning
-ast.body.unshift(esprima.parse('var obj = {"resolution" : 0, "gamut" : 0}'));
+ast.body.unshift(esprima.parse('if (!obj){ var obj = {"resolution" : 0, "gamut" : 0, "color_depth" : 0, "cpu" : 0, "contrast" : 0, "memory" : 0, "concurrency" : 0, "hdr" : 0, "inverted_colors" : 0, "color_depth" : 0}; };'));
 
 // add stat display code to end
 ast.body.push(esprima.parse('for (var key in obj){ console.log(`${key} count: ${obj[key]}`);}'));
+
 var out = escodegen.generate(ast);
 
-console.log("\nProcessed JavaScript:\n")
+//console.log("\nProcessed JavaScript:\n")
 console.log(out);
 
 function enter(node){
   if (node.type === 'BlockStatement'){
     blocks.push(node.body);
   }
-  if (node.type === 'Identifier'){
-    if (node.name === 'width'){
-      var curblock = blocks[blocks.length - 1];
-      var toadd = esprima.parse('obj["resolution"] += 1;').body[0];
-      curblock.push(toadd);
-    }
+  var curblock = blocks[blocks.length - 1];
+
+  var toeval = 'curblock.push(toadd);';
+  // if inside a function definition, add line before return statement
+  if (curblock[curblock.length - 1].type === 'ReturnStatement'){
+    toeval = 'curblock.splice(curblock.length - 1, 0, toadd);'
   }
-  if (node.type === 'TemplateElement'){
-    if (node.value.raw === '(color-gamut: '){
-      var curblock = blocks[blocks.length - 1];
+
+  if (node.type === 'Identifier'){
+    if (node.name.includes('height') || node.name.includes('width')){
+      var toadd = esprima.parse('obj["resolution"] += 1;').body[0];
+      eval(toeval);
+    } else if (node.name.includes('oscpu') || node.name.includes('cpuClass') || node.name.includes('platform')) {
+      var toadd = esprima.parse('obj["cpu"] += 1;').body[0];
+      eval(toeval);
+    } else if (node.name.includes('deviceMemory')){
+      var toadd = esprima.parse('obj["memory"] += 1;').body[0];
+      eval(toeval);
+    } else if (node.name.includes('hardwareConcurrency')){
+      var toadd = esprima.parse('obj["concurrency"] += 1;').body[0];
+      eval(toeval);
+    } else if (node.name.includes('colorDepth')){
+      var toadd = esprima.parse('obj["color_depth"] += 1;').body[0];
+      eval(toeval);
+    }
+  } else if (node.type === 'TemplateElement'){
+    if (node.value.raw.includes('color-gamut')){
       var toadd = esprima.parse('obj["gamut"] += 1;').body[0];
-      curblock.push(toadd);
+      eval(toeval);
+    } else if (node.value.raw.includes('prefers-contrast')){
+      var toadd = esprima.parse('obj["contrast"] += 1;').body[0];
+      eval(toeval);
+    } else if (node.value.raw.includes('dynamic-range')){
+      var toadd = esprima.parse('obj["hdr"] += 1;').body[0];
+      eval(toeval);
+    } else if (node.value.raw.includes('inverted-colors')){
+      var toadd = esprima.parse('obj["inverted_colors"] += 1;').body[0];
+      eval(toeval);
     }
   }
 }
